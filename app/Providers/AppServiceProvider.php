6dfa6019df32ebe0b992a06249f61d3957ee8739
waonpad <waonpad@gmail.com>
2025-02-15 03:04:17 +0900

    n1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Application;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\ServiceProvider;
 
@@ -19,6 +21,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        /** @var Application $app */
+        $app = $this->app;
+
+        /*
+         * N+1を検出する
+         *
+         * @see [Laravelプロジェクトの初期設定 #PHP - Qiita](https://qiita.com/ucan-lab/items/8eab84e37421f907dea0#n1%E6%A4%9C%E5%87%BA%E8%A8%AD%E5%AE%9A)
+         */
+        Model::shouldBeStrict(!$app->isProduction());
+
         // dataプロパティでデータをラップしないようにする
         JsonResource::withoutWrapping();
     }
